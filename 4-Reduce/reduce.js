3650f7cb61e14878361c0006f66942aa7cea13b0
Anthony Rosario <anthonymrosario225@gmail.com>
2021-03-23 22:22:02 -0700

    passing test for reduce function

--- 4-Reduce/reduce.js
+++ 4-Reduce/reduce.js
@@ -1,5 +1,12 @@
 const reduce = (arr, callback, initialValue) => {
-  
+  let accumulator = 0;
+  if(initialValue){
+    accumulator = initialValue;
+  }
+  for(let i = 0; i < arr.length; i++) {
+    accumulator = callback(accumulator, arr[i])
+  }
+  return accumulator;
 }
 
 module.exports = { reduce };
--- 4-Reduce/reduce.test.js
+++ 4-Reduce/reduce.test.js
@@ -1,9 +1,17 @@
-const reduce = require('./reduce.js');
+const { reduce } = require('./reduce.js');
 
 describe('function reduce', () => {
-  it('should take in an array and callback ', () => {
-    expect(
-      reduce()
-    )
-  })
-})
+  const numbers = [1, 4, 6, 12]
+  it('should take in an array of numbers and a callback and return the sum of the array', () => {
+    const sum = (accumulator, item) => accumulator += item;
+
+    expect(reduce(numbers, sum)).toEqual(23)
+  });
+
+  it('should have an initialValue for the accumulator', () => {
+    const sum = (accumulator, item) => accumulator += item;
+    const initialValue = 7;
+    
+    expect(reduce(numbers, sum, initialValue)).toEqual(30);
+  });
+});
