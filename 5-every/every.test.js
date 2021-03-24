6d551a85ff5d231a1a53587a344cab1427a9f4ff
Anthony Rosario <anthonymrosario225@gmail.com>
2021-03-23 22:52:08 -0700

    fixed test for every

--- 5-every/every.test.js
+++ 5-every/every.test.js
@@ -1,7 +1,7 @@
 const { every } = require('./every.js');
 
 describe('function every', () => {
-  it('', () => {
+  it('should take in an array and a callback and return true if the criteria is met and false if otherwise', () => {
     expect(
       every(['apple', 'strawberry', 'banana', 'orange'], item => {
         item.includes('w');
